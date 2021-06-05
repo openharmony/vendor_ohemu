22e91927440cf6df09000a7e4d324d4f92b466bf
SimonLi <likailong@huawei.com>
2021-06-06 04:06:09 +0800

    fix(build): 修正display_qemu_liteos_a的编译

---
+++ display_qemu_liteos_a/patches/patch.py
@@ -0,0 +1,19 @@
+import shutil
+import sys
+import os
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        root_dir = sys.argv[1]
+        root_dir = os.path.join(os.getcwd(), root_dir)
+        shutil.copy(os.path.join(root_dir, "vendor/huawei/display_qemu/patches/kernel_BUILD.gn"),
+                    os.path.join(root_dir, "kernel/liteos_a/BUILD.gn"))
+        shutil.copy(os.path.join(root_dir, "vendor/huawei/display_qemu/patches/build.sh"),
+                    os.path.join(root_dir, "device/qemu/arm_virt/build/build.sh"))
+        shutil.copy(os.path.join(root_dir, "vendor/huawei/display_qemu/patches/BUILD.gn"),
+                    os.path.join(root_dir, "device/qemu/arm_virt/BUILD.gn"))
+        shutil.copy(os.path.join(root_dir, "device/hisilicon/drivers/libs/ohos/llvm/hi3518ev300/libmtd_common.a"), 
+                    os.path.join(root_dir, "device/qemu/drivers/libs/virt/"))
+        shutil.copy(os.path.join(root_dir, "vendor/huawei/display_qemu/patches/qemu-init"), root_dir)
+        shutil.copy(os.path.join(root_dir, "vendor/huawei/display_qemu/patches/qemu-run"), root_dir)
+
