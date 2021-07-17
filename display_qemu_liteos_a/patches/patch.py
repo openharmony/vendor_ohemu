cd7326de3a8e9740ceb958d720c52cb948f8dad4
SimonLi <likailong@huawei.com>
2021-07-17 17:16:46 +0800

    build(graphic): 新增图形的编译
    Signed-off-by: SimonLi <likailong@huawei.com>

--- display_qemu_liteos_a/patches/patch.py
+++ display_qemu_liteos_a/patches/patch.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         root_dir = sys.argv[1]
         root_dir = os.path.join(os.getcwd(), root_dir)
-        shutil.copy(os.path.join(root_dir, "vendor/ohos/display_qemu_liteos_a/patches/build.sh"),
-                    os.path.join(root_dir, "device/qemu/arm_virt/build/build.sh"))
+        shutil.copy(os.path.join(root_dir, "device/hisilicon/drivers/libs/ohos/llvm/hi3518ev300/libspinor_flash.a"),
+                    os.path.join(root_dir, "device/qemu/drivers/libs/virt/libspinor_flash.a"))
         shutil.copy(os.path.join(root_dir, "vendor/ohos/display_qemu_liteos_a/patches/BUILD.gn"),
                     os.path.join(root_dir, "device/qemu/arm_virt/BUILD.gn"))
         shutil.copy(os.path.join(root_dir, "vendor/ohos/display_qemu_liteos_a/patches/qemu-init"), root_dir)
