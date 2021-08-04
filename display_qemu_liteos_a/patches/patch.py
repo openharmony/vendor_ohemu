3807c780910b6bd002279b816a504765a39a9106
SimonLi <likailong@huawei.com>
2021-08-04 15:47:23 +0800

    feat(simple_ui_demo): 新增编译一个simple_ui_demo的应用
    1. 其中字体文件被打包到userfs中，userfs的根目录为storage，所以需要修改
    graphic_config.h的配置文件
    
    2. 新增一个图形的demo样例
    
    close #I43V4Y
    Signed-off-by: SimonLi <likailong@huawei.com>

--- display_qemu_liteos_a/patches/patch.py
+++ display_qemu_liteos_a/patches/patch.py
@@ -2,6 +2,8 @@ import shutil
 import sys
 import os
 
+graphic_config_path = 'foundation/graphic/utils/interfaces/innerkits/graphic_config.h'
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         root_dir = sys.argv[1]
@@ -9,4 +11,17 @@ if __name__ == '__main__':
         shutil.copy(os.path.join(root_dir, "device/hisilicon/drivers/libs/ohos/llvm/hi3518ev300/libspinor_flash.a"),
                     os.path.join(root_dir, "device/qemu/drivers/libs/virt/libspinor_flash.a"))
         shutil.copy(os.path.join(root_dir, "vendor/ohemu/display_qemu_liteos_a/patches/qemu-run"), root_dir)
+
+        graphic_config_abs_path = os.path.join(root_dir, graphic_config_path)
+        print(graphic_config_abs_path)
+        if os.path.isfile(graphic_config_abs_path):
+            with open(graphic_config_abs_path, 'r+') as f:
+                flist = f.readlines()
+                for i in range(len(flist)):
+                    if 'static constexpr const char* VECTOR_FONT_DIR = "/user/data/"' in flist[i]:
+                        print('='*40 + ' find it ' + '='*40)
+                        flist[i] = 'static constexpr const char* VECTOR_FONT_DIR = "/storage/data/";\n'
+                        break
+                with open(graphic_config_abs_path, 'w+') as nf:
+                    nf.writelines(flist)
 
