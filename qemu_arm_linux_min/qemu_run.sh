d0e033afcb477bba90fa31b830788f92ceb9a757
handyohos <zhangxiaotian@huawei.com>
2022-05-27 17:25:00 +0800

    feat:更新arm_virt的qemu_run.sh脚本，支持网络和SN设置，并支持多实例运行
    1）支持-n选项设置创建虚拟网桥
    2）默认可通过kernel启动时多bootargs传递SN号给虚拟机
    3）支持-i选项通过指定的实例运行，每个实例具有不同的MAC地址和SN号
    
    Signed-off-by: handyohos <zhangxiaotian@huawei.com>
    Change-Id: I1bcfffb9523b5b396af236df680987706e55f59e

--- qemu_arm_linux_min/qemu_run.sh
+++ qemu_arm_linux_min/qemu_run.sh
@@ -13,57 +13,153 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-set -e
-elf_file=$1
-rebuild_image=$2
-vnc_enable=$3
-add_boot_args=$4
-boot_args=$5
-net_enable=$6
-gdb_enable=$7
-qemu_test=$8
-test_file=$9
-qemu_help=${10}
+qemu_option="-M virt -cpu cortex-a7 -smp 4 -m 1024 -nographic"
+qemu_setup_network=""
+qemu_instance_id=""
 
-vnc="-vnc :20  -serial mon:stdio"
-qemu_option=""
+kernel_bootargs="console=ttyAMA0,115200 init=/bin/init hardware=qemu.arm.linux default_boot_device=a003e00.virtio_mmio root=/dev/ram0 rw"
 
-if [ "$elf_file" = "" ]; then
-    elf_file=out/qemu-arm-linux/packages/phone/images
+elf_file="out/qemu-arm-linux/packages/phone/images"
+if [ -f "${PWD}/ramdisk.img" ]; then
+  elf_file="${PWD}"
 fi
 
 help_info=$(cat <<-END
 Usage: qemu-run [OPTION]...
 Run a OHOS image in qemu according to the options.
+    -e,  --exec image_path    images path, including: zImage-dtb, ramdisk.img, system.img, vendor.img, userdata.img
+    -g,  --gdb                enable gdb for kernel.
+    -n,  --network            auto setup network for qemu (sudo required).
+    -i,  --instance id        start qemu images with specified instance id (from 01 to 99).
+                              it will also setup network when running in multiple instance mode.
+    -h,  --help               print this help info.
 
-    Options:
+    If no image_path specified, it will find OHOS image in current working directory; then try ${elf_file}.
 
-    -e,  --exec image_path    build images path, including: zImage-dtb, ramdisk.img, system.img, vendor.img, userdata.img
-    -g,  --gdb                enable gdb for kernel
-    -h,  --help               print help info
+    When setting up network, it will create br0 on the host PC with the following information:
+        IP address: 192.168.100.1
+        netmask: 255.255.255.0
 
-    By default, the kernel exec file is: ${elf_file}.
+    The default qemu device MAC address is [00:22:33:44:55:66], default serial number is [0023456789].
+    When running in multiple instances mode, the MAC address and serial number will increase with specified instance ID as follow:
+        MAC address:    {instanceID}:22:33:44:55:66
+        Serial number:  {instanceID}23456789
 END
 )
 
-if [ "$qemu_help" = "yes" ]; then
+function echo_help()
+{
     echo "${help_info}"
     exit 0
-fi
-
-if [ "$gdb_enable" = "yes" ]; then
-    qemu_option+="-s -S"
-fi
-
-function start_qemu(){
-    qemu-system-arm -M virt -cpu cortex-a7 -smp 4 -m 1024 -nographic \
-    $qemu_option \
-    -drive if=none,file=$elf_file/userdata.img,format=raw,id=userdata,index=3 -device virtio-blk-device,drive=userdata \
-    -drive if=none,file=$elf_file/vendor.img,format=raw,id=vendor,index=2 -device virtio-blk-device,drive=vendor \
-    -drive if=none,file=$elf_file/system.img,format=raw,id=system,index=1 -device virtio-blk-device,drive=system \
-    -drive if=none,file=$elf_file/updater.img,format=raw,id=updater,index=0 -device virtio-blk-device,drive=updater \
-    -kernel $elf_file/zImage-dtb -initrd $elf_file/ramdisk.img \
-    -append "console=ttyAMA0,115200 init=/bin/init hardware=qemu.arm.linux default_boot_device=a003e00.virtio_mmio root=/dev/ram0 rw"
 }
 
-start_qemu
+function qemu_option_add()
+{
+    qemu_option+=" "
+    qemu_option+="$1"
+}
+
+function kernel_bootargs_add()
+{
+    kernel_bootargs+=" "
+    kernel_bootargs+="$1"
+}
+
+function setup_sn()
+{
+    if [ x"${qemu_instance_id}" != x ]; then
+        kernel_bootargs_add "ohos.boot.sn=${qemu_instance_id}23456789"
+    else
+        kernel_bootargs_add "ohos.boot.sn=0023456789"
+    fi
+}
+
+function parameter_verification()
+{
+    if [ $1 -eq 0 ] || [ x"$(echo $2 | cut -c 1)" = x"-" ]; then
+        echo_help
+    fi
+}
+
+function qemu_network()
+{
+    ifconfig br0 > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        qemu_install_path=`which qemu-system-arm`
+        qemu_install_path=`dirname ${qemu_install_path}`
+        qemu_install_path=${qemu_install_path}/../etc/qemu
+        [ ! -d ${qemu_install_path} ] && sudo mkdir -p ${qemu_install_path}
+        echo 'allow br0' | sudo tee -a ${qemu_install_path}/bridge.conf
+        [ ! -d /etc/qemu ] && sudo mkdir -p /etc/qemu
+        echo 'allow br0' | sudo tee -a /etc/qemu/bridge.conf
+        sudo modprobe tun tap
+        sudo ip link add br0 type bridge
+        sudo ip address add 192.168.100.1/24 dev br0
+        sudo ip link set dev br0 up
+    fi
+
+    [ x"${qemu_instance_id}" != x ] && qemu_option_add "-netdev bridge,id=net0 -device virtio-net-device,netdev=net0,mac=${qemu_instance_id}:22:33:44:55:66"
+    [ x"${qemu_instance_id}" == x ] && qemu_option_add "-netdev bridge,id=net0 -device virtio-net-device,netdev=net0,mac=00:22:33:44:55:66"
+    qemu_option_add "-no-user-config"
+}
+
+function copy_img()
+{
+    cp $elf_file/userdata.img $elf_file/userdata${qemu_instance_id}.img
+    cp $elf_file/vendor.img $elf_file/vendor${qemu_instance_id}.img
+    cp $elf_file/system.img $elf_file/system${qemu_instance_id}.img
+    cp $elf_file/updater.img $elf_file/updater${qemu_instance_id}.img
+}
+
+while [ $# -ne 0 ]
+do
+    case $1 in
+        "-e")
+            shift
+            parameter_verification $# $1
+            elf_file="$1"
+            ;;
+        "-i")
+            shift
+            parameter_verification $# $1
+            qemu_instance_id=$1
+            # Setup qemu network by default when running in multi instance mode
+            qemu_setup_network="y"
+            ;;
+        "-n")
+            qemu_setup_network="y"
+            ;;
+        "-g")
+            qemu_option_add "-s -S"
+            ;;
+        "-h")
+            echo_help
+            ;;
+    esac
+
+    shift
+done
+
+if [ ! -f $elf_file/ramdisk.img ]; then
+  echo_help
+fi
+
+# Setup qemu network if
+[ x"${qemu_setup_network}" != x ] && qemu_network
+
+# Copy original images for multi instance running
+[ x"${qemu_instance_id}" != x ] && copy_img
+
+setup_sn
+
+qemu_option_add "-drive if=none,file=$elf_file/userdata${qemu_instance_id}.img,format=raw,id=userdata,index=3 -device virtio-blk-device,drive=userdata"
+qemu_option_add "-drive if=none,file=$elf_file/vendor${qemu_instance_id}.img,format=raw,id=vendor,index=2 -device virtio-blk-device,drive=vendor"
+qemu_option_add "-drive if=none,file=$elf_file/system${qemu_instance_id}.img,format=raw,id=system,index=1 -device virtio-blk-device,drive=system"
+qemu_option_add "-drive if=none,file=$elf_file/updater${qemu_instance_id}.img,format=raw,id=updater,index=0 -device virtio-blk-device,drive=updater"
+qemu_option_add "-kernel $elf_file/zImage-dtb -initrd $elf_file/ramdisk.img"
+
+# Setup network need sudo
+[ x"${qemu_setup_network}" != x ] && sudo qemu-system-arm ${qemu_option} -append "${kernel_bootargs}"
+
+# start without sudo if no need to setup network
+[ x"${qemu_setup_network}" == x ] && qemu-system-arm ${qemu_option} -append "${kernel_bootargs}"
