2a53257f12ccf896c30aa2c7686beed0ece4952d
chengjinsong2 <chengjinsong2@huawei.com>
2022-09-13 00:09:03 -0700

    删除hal_sys_param.c多余接口
    Signed-off-by: chengjinsong2 <chengjinsong2@huawei.com>
    Change-Id: I65a8bacbb2922d3ba69be5e5c1e6ec6a970d897e

--- qemu_xtensa_mini_system_demo/hals/utils/sys_param/hal_sys_param.c
+++ qemu_xtensa_mini_system_demo/hals/utils/sys_param/hal_sys_param.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,112 +15,9 @@
 
 #include "hal_sys_param.h"
 
-#define OHOS_DEVICE_TYPE "****"
-#define OHOS_DISPLAY_VERSION "OpenHarmony 3.0.1"
-#define OHOS_MANUFACTURE "****"
-#define OHOS_BRAND "****"
-#define OHOS_MARKET_NAME "****"
-#define OHOS_PRODUCT_SERIES "****"
-#define OHOS_PRODUCT_MODEL "****"
-#define OHOS_SOFTWARE_MODEL "****"
-#define OHOS_HARDWARE_MODEL "****"
-#define OHOS_HARDWARE_PROFILE "aout:true,display:true"
-#define OHOS_BOOTLOADER_VERSION "bootloader"
-#define OHOS_ABI_LIST "****"
-#define OHOS_SERIAL "1234567890"
-#define OHOS_FIRST_API_VERSION 1
+static const char OHOS_SERIAL[] = {"1234567890"};  // provided by OEM.
 
-const char *HalGetDeviceType(void)
-{
-    return OHOS_DEVICE_TYPE;
-}
-
-const char *HalGetManufacture(void)
-{
-    return OHOS_MANUFACTURE;
-}
-
-const char *HalGetBrand(void)
-{
-    return OHOS_BRAND;
-}
-
-const char *HalGetMarketName(void)
-{
-    return OHOS_MARKET_NAME;
-}
-
-const char *HalGetProductSeries(void)
-{
-    return OHOS_PRODUCT_SERIES;
-}
-
-const char *HalGetProductModel(void)
-{
-    return OHOS_PRODUCT_MODEL;
-}
-
-const char *HalGetSoftwareModel(void)
-{
-    return OHOS_SOFTWARE_MODEL;
-}
-
-const char *HalGetHardwareModel(void)
-{
-    return OHOS_HARDWARE_MODEL;
-}
-
-const char *HalGetHardwareProfile(void)
-{
-    return OHOS_HARDWARE_PROFILE;
-}
-
-const char *HalGetSerial(void)
+const char* HalGetSerial(void)
 {
     return OHOS_SERIAL;
-}
-
-const char *HalGetBootloaderVersion(void)
-{
-    return OHOS_BOOTLOADER_VERSION;
-}
-
-const char *HalGetAbiList(void)
-{
-    return OHOS_ABI_LIST;
-}
-
-const char *HalGetDisplayVersion(void)
-{
-    return OHOS_DISPLAY_VERSION;
-}
-
-const char *HalGetIncrementalVersion(void)
-{
-    return INCREMENTAL_VERSION;
-}
-
-const char *HalGetBuildType(void)
-{
-    return BUILD_TYPE;
-}
-
-const char *HalGetBuildUser(void)
-{
-    return BUILD_USER;
-}
-
-const char *HalGetBuildHost(void)
-{
-    return BUILD_HOST;
-}
-
-const char *HalGetBuildTime(void)
-{
-    return BUILD_TIME;
-}
-
-int HalGetFirstApiVersion(void)
-{
-    return OHOS_FIRST_API_VERSION;
 }
